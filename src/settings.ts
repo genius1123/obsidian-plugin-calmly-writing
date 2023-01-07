98f725de97213e54474e5505116aad222b977a70
sz <1911392294@qq.com>
2023-01-07 11:47:05 +0800

    [fix]整理setiings

--- src/settings.ts
+++ src/settings.ts
@@ -1,28 +1,18 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import OpenVSCode from './main';
+import TypewriterSound from './main';
 
-export interface OpenVSCodeSettings {
-	ribbonIcon: boolean;
-	ribbonCommandUsesCode: boolean;
-	executeTemplate: string;
-	openFile: boolean;
-	workspacePath: string;
-	useUrlInsiders: boolean;
+export interface TypewriterSoundSettings {
+	isTypewriterSoundOpen: boolean;
 }
 
-export const DEFAULT_SETTINGS: OpenVSCodeSettings = {
-	ribbonIcon: true,
-	ribbonCommandUsesCode: true,
-	executeTemplate: 'code "{{vaultpath}}" "{{vaultpath}}/{{filepath}}"',
-	openFile: true,
-	workspacePath: '{{vaultpath}}',
-	useUrlInsiders: false,
+export const DEFAULT_SETTINGS: TypewriterSoundSettings = {
+	isTypewriterSoundOpen: true,
 };
 
-export class OpenVSCodeSettingsTab extends PluginSettingTab {
-	plugin: OpenVSCode;
+export class TypewriterSoundSettingsTab extends PluginSettingTab {
+	plugin: TypewriterSound;
 
-	constructor(app: App, plugin: OpenVSCode) {
+	constructor(app: App, plugin: TypewriterSound) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -32,108 +22,15 @@ export class OpenVSCodeSettingsTab extends PluginSettingTab {
 		containerEl.empty();
 		containerEl.createEl('h3', { text: 'General settings' });
 
+		// 切换icon图标
 		new Setting(containerEl)
-			.setName('Display Ribbon Icon')
-			.setDesc('Toggle this OFF if you want to hide the Ribbon Icon.')
+			.setName('Toggle')
+			.setDesc('Toggle this OFF if you want to close it')
 			.addToggle((toggle) =>
-				toggle.setValue(this.plugin.settings.ribbonIcon).onChange((value) => {
-					this.plugin.settings.ribbonIcon = value;
-					this.plugin.saveSettings();
-					this.plugin.refreshIconRibbon();
-				}),
-			);
-
-		new Setting(containerEl)
-			.setName('Ribbon opens via `code` command')
-			.setDesc(`Toggle this OFF if you'd prefer that the Ribbon Icon opens VSCode via URL.`)
-			.addToggle((toggle) =>
-				toggle.setValue(this.plugin.settings.ribbonCommandUsesCode).onChange((value) => {
-					this.plugin.settings.ribbonCommandUsesCode = value;
+				toggle.setValue(this.plugin.settings.isTypewriterSoundOpen).onChange((value) => {
+					this.plugin.settings.isTypewriterSoundOpen = value;
 					this.plugin.saveSettings();
 				}),
 			);
-
-		containerEl.createEl('h3', { text: 'Open via `code` CLI settings' });
-
-		new Setting(containerEl)
-			.setName('Template for executing the `code` command')
-			.setDesc(
-				'You can use the following variables: `{{vaultpath}}` (absolute), `{{filepath}}` (relative). Note that on MacOS, a full path to the VSCode executable is required (generally "/usr/local/bin/code"). Example: `/usr/local/bin/code "{{vaultpath}}" "{{vaultpath}}/{{filepath}}"`',
-			)
-			.addText((text) =>
-				text
-					.setPlaceholder(DEFAULT_SETTINGS.executeTemplate)
-					.setValue(this.plugin.settings.executeTemplate || DEFAULT_SETTINGS.executeTemplate)
-					.onChange((value) => {
-						value = value.trim();
-						if (value === '') value = DEFAULT_SETTINGS.executeTemplate;
-						this.plugin.settings.executeTemplate = value;
-						this.plugin.saveData(this.plugin.settings);
-					}),
-			);
-
-		containerEl.createEl('h3', { text: 'Open via `vscode://` URL settings' });
-
-		const openViaUrlCaveat = containerEl.createEl('p');
-		const openViaUrlCaveatEm = openViaUrlCaveat.createEl('em', {
-			text: `
-				⚠️ This setting is not recommended for Windows users due to
-				UX issues caused by security enhancements in VSCode on Windows.
-				More information:
-			`,
-		});
-		openViaUrlCaveatEm.appendChild(
-			createEl('a', {
-				text: 'Open in VSCode Readme',
-				href: 'https://github.com/NomarCub/obsidian-open-vscode/blob/master/README.md#caveats-regarding-the-url-command-for-windows-users',
-			}),
-		);
-		openViaUrlCaveatEm.appendText('.');
-
-		new Setting(containerEl)
-			.setName('Open current file')
-			.setDesc('Open the current file rather than the root of the vault.')
-			.addToggle((toggle) =>
-				toggle.setValue(this.plugin.settings.openFile || DEFAULT_SETTINGS.openFile).onChange((value) => {
-					this.plugin.settings.openFile = value;
-					this.plugin.saveData(this.plugin.settings);
-				}),
-			);
-
-		new Setting(containerEl)
-			.setName('Path to VSCode Workspace')
-			.setDesc(
-				'Defaults to the {{vaultpath}} template variable. You can set this to an absolute path to a ".code-workspace" file if you prefer to use a Multi Root workspace file: ',
-			)
-			.setClass('setting-item--vscode-workspacePath')
-			.addText((text) =>
-				text
-					.setPlaceholder(DEFAULT_SETTINGS.workspacePath)
-					.setValue(this.plugin.settings.workspacePath || DEFAULT_SETTINGS.workspacePath)
-					.onChange((value) => {
-						value = value.trim();
-						if (value === '') value = DEFAULT_SETTINGS.workspacePath;
-						this.plugin.settings.workspacePath = value;
-						this.plugin.saveData(this.plugin.settings);
-					}),
-			);
-
-		const workspacePathDescEl = containerEl.querySelector(
-			'.setting-item--vscode-workspacePath .setting-item-description',
-		);
-		workspacePathDescEl!.appendChild(
-			createEl('a', {
-				href: 'https://code.visualstudio.com/docs/editor/workspaces#_multiroot-workspaces',
-				text: 'https://code.visualstudio.com/docs/editor/workspaces#_multiroot-workspaces',
-			}),
-		);
-		workspacePathDescEl!.appendText('.');
-
-		new Setting(containerEl).setName('Open VSCode using a `vscode-insiders://` URL').addToggle((toggle) => {
-			toggle.setValue(this.plugin.settings.useUrlInsiders).onChange((value) => {
-				this.plugin.settings.useUrlInsiders = value;
-				this.plugin.saveSettings();
-			});
-		});
 	}
 }
