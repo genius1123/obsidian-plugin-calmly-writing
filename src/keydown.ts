46e3b958b69457ba74eb3657acecda8595773968
sz <1911392294@qq.com>
2023-01-09 16:30:57 +0800

    [feat]新增一些声音，onkeydown出现声音

--- src/keydown.ts
+++ src/keydown.ts
@@ -1,5 +1,42 @@
+
+import * as path from 'path';
+import player, { PlayerConfig } from './player';
+
+const _basePath: string = app.vault.adapter.getBasePath() + "\\.obsidian\\plugins\\calmly-writing\\src"
+
+// const returnNew: string = path.join(_basePath, 'sound', 'return-new.mp3');
+// const _deleteAudio: string = path.join(_basePath, 'sound', 'delete.mp3');
+// const _otherKeysAudio: string = path.join(_basePath, 'sound', 'key.mp3');
+// const _cutAudio: string = path.join(_basePath, 'sound', 'cut.mp3');
+// const _pasteAudio: string = path.join(_basePath, 'sound', 'paste.mp3');
+// const _enterAudio: string = path.join(_basePath, 'sound', 'enter.mp3');
+// const _tabAudio: string = path.join(_basePath, 'sound', 'tab.mp3');
+// const _arrowsAudio: string = path.join(_basePath, 'sound', 'arrow.mp3');
+
+
+const returnNew: string = path.join(_basePath, 'sound', 'return-new.wav');
+const _deleteAudio: string = path.join(_basePath, 'sound', 'delete.wav');
+const _otherKeysAudio: string = path.join(_basePath, 'sound', 'key.wav');
+const _cutAudio: string = path.join(_basePath, 'sound', 'cut.wav');
+const _pasteAudio: string = path.join(_basePath, 'sound', 'paste.wav');
+const _enterAudio: string = path.join(_basePath, 'sound', 'enter.wav');
+const _tabAudio: string = path.join(_basePath, 'sound', 'tab.wav');
+const _arrowsAudio: string = path.join(_basePath, 'sound', 'arrow.wav');
+
+
+let config: PlayerConfig = {
+    macVol: 1,
+    winVol: 100,
+    linuxVol: 100
+};
+
+
 export const window_onkeydown = function (e) {
 	console.log('====================================');
 	console.log(e);
 	console.log('====================================');
+	console.log(returnNew);
+	console.log('returnNew',returnNew)
+	
+	player.play(_deleteAudio, config);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { App, FileSystemAdapter, Plugin } from 'obsidian';
 import { DEFAULT_SETTINGS, TypewriterSoundSettings, TypewriterSoundSettingsTab } from './settings';
 import { window_onkeydown } from "./keydown";
+import * as path from 'path';
+import player, { PlayerConfig } from './player';
+
 
 export default class TypewriterSound extends Plugin {
 	settings: TypewriterSoundSettings;
 
+    private _basePath: string = this.app.vault.adapter.getBasePath() + "\\.obsidian\\plugins\\calmly-writing\\src"
+
+    private returnNew: string = path.join(this._basePath, 'sound', 'return-new.mp3');
+    private _deleteAudio: string = path.join(this._basePath, 'sound', 'delete.mp3');
+    private _otherKeysAudio: string = path.join(this._basePath, 'sound', 'key.mp3');
+    private _cutAudio: string = path.join(this._basePath, 'sound', 'cut.mp3');
+    private _pasteAudio: string = path.join(this._basePath, 'sound', 'paste.mp3');
+    private _enterAudio: string = path.join(this._basePath, 'sound', 'enter.mp3');
+    private _tabAudio: string = path.join(this._basePath, 'sound', 'tab.mp3');
+    private _arrowsAudio: string = path.join(this._basePath, 'sound', 'arrow.mp3');
 
 	async onload() {
 		this.addSettingTab(new TypewriterSoundSettingsTab(this.app, this));
 		await this.loadSettings();
+		console.log(this._deleteAudio);
+		console.log('player',player)
+		
 
 		document.onkeydown = (e) => { window_onkeydown(e); }
 	}
